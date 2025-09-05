73a87c18f9a2839f07d3beaff1691bfd49094f4b
Ingvild Bjerke <ingvieb@gmail.com>
2025-09-05 11:29:10 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,17 +9,40 @@ def calculate_iou(gt_mask, pred_mask):
     iou = intersection / union if union > 0 else 0
     return iou
 
+def generate_cellpose_npy_dict(masks, outlines, colors, filepath):
+        
+    npy_dict = {'masks': masks,
+                'outlines': outlines,
+                'colors': colors,
+                'filename': str(filepath),
+                'flow_threshold': 0.4,
+                'cellprob_threshold': 0.0,
+                'normalize_params': {'lowhigh': None,
+                'percentile': [1.0, 99.0],
+                'normalize': True,
+                'norm3D': True,
+                'sharpen_radius': 0.0,
+                'smooth_radius': 0.0,
+                'tile_norm_blocksize': 0.0,
+                'tile_norm_smooth3D': 0.0,
+                'invert': False},
+                'restore': None,
+                'ratio': 1.0,
+                'diameter': None}
+    
+    return npy_dict
 
-def split_z_stack(tif_file):
+def split_annotated_z_stack(tif_file):
+    # open tif image
     img = tifffile.TiffFile(tif_file)
-    image_array = img.asarray()
+    image_array = img.asarray()   
+  
+    img_z_list = []
+    npy_z_list = []
     
     # Load the corresponding .npy file
     npy_file = tif_file.parent / f"{tif_file.stem}_seg.npy"
     data = np.load(npy_file, allow_pickle=True).item()
-    
-    img_z_list = []
-    npy_z_list = []
 
     # Loop z planes found in the .tif file
     for z in range(image_array.shape[0]):
@@ -37,23 +60,8 @@ def split_z_stack(tif_file):
         z_colors = [[random.randint(0, 255) for _ in range(3)] for _ in range(mask_number)]
 
         # Create a dictionary to hold the z-plane data and masks
-        npy_z = {
-            'masks': z_masks,          # Update with the current z-plane mask
-            'outlines': z_annos,       # Update with the current z-plane outlines
-            'colors': z_colors,         # Use generated random colors
-            'current_channel': data['current_channel'],
-            'filename': data['filename'],
-            'flows': data['flows'],
-            'zdraw': data['zdraw'],
-            'model_path': data['model_path'],
-            'flow_threshold': data['flow_threshold'],
-            'cellprob_threshold': data['cellprob_threshold'],
-            'normalize_params': data['normalize_params'],
-            'restore': data['restore'],
-            'ratio': data['ratio'],
-            'diameter': data['diameter']
-        }
-
+        npy_z = generate_cellpose_npy_dict(z_masks, z_annos, z_colors, data['filename'])
+       
         img_z_list.append(img_z)
         npy_z_list.append(npy_z)
 
